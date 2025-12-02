66d123b5c001ca67c9a9672b911f8891d584fba8
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2025-12-02 11:25:11 +0000

    add TripDao

---
+++ app/src/main/java/com/example/receipttracker/data/TripDao.kt
@@ -0,0 +1,27 @@
+package com.example.receipttracker.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TripDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(trip: Trip)
+
+    @Update
+    suspend fun update(trip: Trip)
+
+    @Delete
+    suspend fun delete(trip: Trip)
+
+    @Query("SELECT * from trip WHERE trip_id = :id")
+    fun getTrip(id: Int): Flow<Trip>
+
+    @Query("SELECT * from trip ORDER BY start_date ASC")
+    fun getAllTripsByDateAsc(): Flow<List<Trip>>
+}
