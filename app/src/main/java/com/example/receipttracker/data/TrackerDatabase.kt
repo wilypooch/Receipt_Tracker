cf626185032899bb22c7ac5f3faed11750cb5a5e
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2025-12-02 11:49:02 +0000

    add TrackerDatabase

---
+++ app/src/main/java/com/example/receipttracker/data/TrackerDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.receipttracker.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Trip::class, Receipt::class], version = 1)
+abstract class TrackerDatabase : RoomDatabase() {
+    abstract fun tripDao(): TripDao
+    abstract fun receiptDao(): ReceiptDao
+
+    companion object {
+        @Volatile
+        private var Instance: TrackerDatabase? = null
+
+        fun getDatabase(context: Context): TrackerDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, TrackerDatabase::class.java, "tracker_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
