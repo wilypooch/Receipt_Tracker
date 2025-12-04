618ff4e9a6489e6ec89aeb11d2a4e9ec06f00f6a
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2025-12-04 11:54:29 +0000

    add basic trip list UI which is a lazy column consisting of individual trip list cards

---
+++ app/src/main/java/com/example/receipttracker/ui/TripList.kt
@@ -0,0 +1,88 @@
+package com.example.receipttracker.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.receipttracker.data.Trip
+import com.example.receipttracker.ui.theme.ReceiptTrackerTheme
+
+private val tripsForPreview = listOf(
+    Trip(23, "SANS", "01/02/23", endDate = "08/03/1993", 400.00),
+    Trip(24, "Defcon33", "05/08/25", endDate = "08/03/1993", 20.00),
+    Trip(25, "Work Trip", "01/02/25", endDate = "08/03/1993", 4888.86)
+)
+
+@Composable
+fun TripList(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        // TODO: Add view for when list is empty
+        items(tripsForPreview) { item ->
+            TripListCard(
+                item.name,
+                item.startDate,
+                item.totalAmount
+            ) {} //TODO: Do I need to do anything with this lambda??
+        }
+    }
+}
+
+@Composable
+fun TripListCard(tripName: String, tripStartDate: String, tripTotal: Double, onClick: () -> Unit) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        onClick = { TODO() }) {
+        Row {
+            Text(tripName, style = MaterialTheme.typography.titleLarge)
+        }
+        Row {
+            Column(Modifier.weight(2f)) {
+                Text(text = "Start Date:", style = MaterialTheme.typography.labelMedium)
+            }
+            Column {
+                Text(text = tripStartDate, style = MaterialTheme.typography.labelMedium)
+            }
+        }
+        Row {
+            Column(Modifier.weight(2f)) {
+                Text(text = "Total:", style = MaterialTheme.typography.labelMedium)
+            }
+            Column {
+                // TODO: Format currency
+                Text(text = tripTotal.toString(), style = MaterialTheme.typography.labelMedium)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TripListPreview() {
+    ReceiptTrackerTheme {
+        TripList()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TripListCardPreview() {
+    ReceiptTrackerTheme {
+        TripListCard("Sample Name", tripStartDate = "01/01/1970", tripTotal = 500.00, onClick = {})
+    }
+}
