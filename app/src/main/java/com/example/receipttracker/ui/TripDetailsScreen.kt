2bfb75180f00796b55579d6b3fcee8ff590aafbd
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2025-12-05 12:32:32 +0000

    add preview for Details Screen composable

--- app/src/main/java/com/example/receipttracker/ui/TripDetailsScreen.kt
+++ app/src/main/java/com/example/receipttracker/ui/TripDetailsScreen.kt
@@ -2,8 +2,18 @@ package com.example.receipttracker.ui
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.receipttracker.ui.theme.ReceiptTrackerTheme
 
 @Composable
 fun TripDetailScreen(tripId: Int, onNavigateUp: () -> Any?) {
     Text(text = "Trip Details for $tripId")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TripDetailsScreenPreview() {
+    ReceiptTrackerTheme {
+        TripDetailScreen(tripId = 67, onNavigateUp = {})
+    }
 }
