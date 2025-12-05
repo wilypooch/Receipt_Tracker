dd53400c7474b4e208934cae6a1f1da37e84421d
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2025-12-05 11:31:01 +0000

    add Navigation 3 NavDisplay and backstack

--- app/src/main/java/com/example/receipttracker/ReceiptTrackerApp.kt
+++ app/src/main/java/com/example/receipttracker/ReceiptTrackerApp.kt
@@ -1,8 +1,50 @@
 package com.example.receipttracker
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.ui.NavDisplay
+import com.example.receipttracker.ui.HomeScreen
+
+data object TripList
+data class TripDetail(val id: Int)
 
 @Composable
 fun ReceiptTrackerApp() {
-    // TODO: Add NavHost here
+    val backStack = remember { mutableStateListOf<Any>(TripList) }
+
+    NavDisplay(
+        backStack = backStack,
+        onBack = { backStack.removeLastOrNull() },
+        entryProvider = { key ->
+            when (key) {
+                is TripList -> NavEntry(key) {
+                    HomeScreen(
+                        // TODO: perhaps change this ID, it doesn't feel right
+                        onAddTripClick = { backStack.add(TripDetail(-1)) },
+                        onViewTripClick = { selectedTripId ->
+                            backStack.add(
+                                TripDetail(
+                                    selectedTripId
+                                )
+                            )
+                        }
+                    )
+                }
+
+                is TripDetail -> NavEntry(key) {
+                    val tripId = key.id
+                    TripDetailScreen(
+                        tripId = tripId,
+                        onNavigateUp = { backStack.removeLastOrNull() }
+                    )
+                }
+
+                else -> NavEntry(Unit) { Text("Unknown Route") }
+            }
+        }
+    )
+}
 }
