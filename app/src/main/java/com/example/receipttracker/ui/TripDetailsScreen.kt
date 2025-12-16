907f2b1c5e29acb673d8366f4fa806d3991778b3
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2025-12-16 21:19:12 +0000

    fix: make full OutlinedTextField containing the dates clickable by responding to focus changes

--- app/src/main/java/com/example/receipttracker/ui/TripDetailsScreen.kt
+++ app/src/main/java/com/example/receipttracker/ui/TripDetailsScreen.kt
@@ -2,7 +2,6 @@ package com.example.receipttracker.ui
 
 import android.icu.text.SimpleDateFormat
 import android.icu.util.TimeZone
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,6 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -98,6 +99,7 @@ fun TripDetailContent(
         }
     }
     var showDatePicker by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -124,7 +126,12 @@ fun TripDetailContent(
                     )
                 }
             },
-            modifier = Modifier.clickable { showDatePicker = true }
+            modifier = Modifier.onFocusChanged { focusState ->
+                if (focusState.isFocused) {
+                    showDatePicker = true
+                    focusManager.clearFocus()
+                }
+            }
         )
 
         OutlinedTextField(
@@ -140,7 +147,12 @@ fun TripDetailContent(
                     )
                 }
             },
-            modifier = Modifier.clickable { showDatePicker = true }
+            modifier = Modifier.onFocusChanged { focusState ->
+                if (focusState.isFocused) {
+                    showDatePicker = true
+                    focusManager.clearFocus()
+                }
+            }
         )
 
         OutlinedTextField(
