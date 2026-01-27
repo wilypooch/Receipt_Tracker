66ed44c03d0b3d87c60d7af12e2c6e5735fc5375
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-01-27 16:51:00 +0000

    add receipt type drop down

---
+++ app/src/main/java/com/example/receipttracker/ui/utils/ReceiptTypeDropDown.kt
@@ -0,0 +1,59 @@
+package com.example.receipttracker.ui.utils
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.example.receipttracker.data.ReceiptType
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReceiptTypeDropdown(
+    selectedReceiptType: ReceiptType,
+    onReceiptTypeSelected: (ReceiptType) -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }
+    ) {
+        OutlinedTextField(
+            value = selectedReceiptType.displayName,
+            onValueChange = {},
+            readOnly = true,
+            label = { Text("Receipt Type") },
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
+            modifier = Modifier
+                .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable, true)
+                .fillMaxWidth()
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            ReceiptType.entries.forEach { receiptType ->
+                DropdownMenuItem(
+                    text = { Text(receiptType.displayName) },
+                    onClick = {
+                        onReceiptTypeSelected(receiptType)
+                        expanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                )
+            }
+        }
+    }
+}
