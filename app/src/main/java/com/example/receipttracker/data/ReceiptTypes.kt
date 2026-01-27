23963bc9fe373838c823bec71a2c632f55dafa58
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-01-27 16:50:41 +0000

    add receipt type enum class

---
+++ app/src/main/java/com/example/receipttracker/data/ReceiptTypes.kt
@@ -0,0 +1,13 @@
+package com.example.receipttracker.data
+
+enum class ReceiptType(val displayName: String) {
+    FoodAndDrink("Food And Drink"),
+    CarHire("Car Hire"),
+    Flight("Flight"),
+    Fuel("Fuel"),
+    Hotel("Hotel"),
+    Parking("Parking"),
+    Rail("Rail"),
+    Taxi("Taxi"),
+    Other("Other")
+}
