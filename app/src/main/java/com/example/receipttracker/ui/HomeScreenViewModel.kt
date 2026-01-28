1e8ad7c5b2a7b9be5bc7ed18713be84a0a1c53bd
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-01-28 13:55:43 +0000

    remove redundant deleteTrip function and redundant val constructor parameter

--- app/src/main/java/com/example/receipttracker/ui/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/receipttracker/ui/HomeScreenViewModel.kt
@@ -8,29 +8,22 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 data class HomeUiState(
     val trips: List<Trip> = emptyList(),
 )
 
 class HomeViewModel(
-    private val repository: TrackerRepository,
+    repository: TrackerRepository,
 ) : ViewModel() {
 
     val uiState: StateFlow<HomeUiState> =
-        repository.getAllTripsByDateAscStream()                          // Flow<List<Trip>
+        repository.getAllTripsByDateAscStream()
             .map { trips -> HomeUiState(trips = trips) }
             .stateIn(
                 viewModelScope,
                 SharingStarted.WhileSubscribed(5_000),
                 HomeUiState()
             )
-
-    fun deleteTrip(trip: Trip) {
-        viewModelScope.launch {
-            repository.deleteTripById(trip.tripId)
-        }
-    }
 }
 
