0f1176a518a9d3ed38522a8d77f268bb624f6d68
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-01-30 15:59:24 +0000

    standardise scrolling across edit receipt and edit trip screens

--- app/src/main/java/com/example/receipttracker/ui/EditReceiptDataScreen.kt
+++ app/src/main/java/com/example/receipttracker/ui/EditReceiptDataScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
@@ -140,6 +142,7 @@ fun EditReceiptDataScreen(
         )
     }
     ) { innerPadding ->
+        val scrollState = rememberScrollState()
         if (uiState == null) {
             Box(
                 Modifier.fillMaxSize(),
@@ -148,11 +151,12 @@ fun EditReceiptDataScreen(
                 CircularProgressIndicator()
             }
         } else {
-            // TODO: Add functionality to deal with screen rotation / different screen sizes
+            // TODO: Add functionality to deal with different screen sizes
             Column(
                 modifier = Modifier
                     .padding(innerPadding)
                     .fillMaxSize()
+                    .verticalScroll(scrollState)
             ) {
                 EditReceiptDataContent(
                     receipt = receiptToDisplay!!,
