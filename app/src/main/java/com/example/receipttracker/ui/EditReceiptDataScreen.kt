3a08f4f8e21cbfcc8b7ee1bcf7258c7004a841df
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-01-30 17:29:14 +0000

    enlarge async image of receipt in edit screen to a given maximum height whilst filling max width

--- app/src/main/java/com/example/receipttracker/ui/EditReceiptDataScreen.kt
+++ app/src/main/java/com/example/receipttracker/ui/EditReceiptDataScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -198,7 +200,8 @@ fun EditReceiptDataContent(
             model = receipt.imageUri,
             contentDescription = "Receipt Image",
             modifier = Modifier
-                .height(400.dp)
+                .fillMaxWidth()
+                .heightIn(max = if (isExpanded) 600.dp else 400.dp)
         )
     }
 
