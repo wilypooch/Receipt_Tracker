657c661ebf807a0d6e473d8da0bc28a76c517b66
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-02-04 14:01:29 +0000

    fix entry provider DSL implementation

--- app/src/main/java/com/example/receipttracker/ReceiptTrackerApp.kt
+++ app/src/main/java/com/example/receipttracker/ReceiptTrackerApp.kt
@@ -2,13 +2,13 @@ package com.example.receipttracker
 
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
@@ -25,34 +25,33 @@ import kotlinx.serialization.Serializable
 import java.util.UUID
 
 @Serializable
-sealed interface Route : NavKey {
+sealed interface Route : NavKey
 
-    @Serializable
-    data object TripList : Route, NavKey
+@Serializable
+data object TripList : Route
 
-    @Serializable
-    data class TripOverview(val id: Int) : Route, NavKey
+@Serializable
+data class TripOverview(val id: Int) : Route
 
-    @Serializable
-    data class EditTrip(val id: Int) : Route, NavKey
+@Serializable
+data class EditTrip(val id: Int) : Route
 
-    @Serializable
-    data class AddReceipt(val tripId: Int, val currencyCode: String) : Route, NavKey
+@Serializable
+data class AddReceipt(val tripId: Int, val currencyCode: String) : Route
 
-    @Serializable
-    data class EditReceipt(
-        val receiptId: Int,
-        val tripStartDate: String,
-        val tripEndDate: String,
-        val currencyCode: String,
-    ) : Route, NavKey
-}
+@Serializable
+data class EditReceipt(
+    val receiptId: Int,
+    val tripStartDate: String,
+    val tripEndDate: String,
+    val currencyCode: String,
+) : Route
 
 
 @Suppress("UNCHECKED_CAST")
 @Composable
 fun ReceiptTrackerApp(windowSize: WindowWidthSizeClass) {
-    val backStack = rememberNavBackStack(Route.TripList)
+    val backStack = rememberNavBackStack(TripList)
     var snackbarMessage by rememberSaveable { mutableStateOf<String?>(null) }
     val context = LocalContext.current
     val application = context.applicationContext as ReceiptTrackerApplication
@@ -62,152 +61,143 @@ fun ReceiptTrackerApp(windowSize: WindowWidthSizeClass) {
         // TODO: fix this onBack implementation as it is not currently used within the entry provider and code is duplicated.
         onBack = { backStack.removeLastOrNull() },
         entryProvider = entryProvider {
-            entry<Route.TripList> { key ->
-                NavEntry(key) {
-                    val homeViewModel: HomeViewModel = viewModel(
-                        factory = HomeViewModel.provideFactory(repository)
-                    )
-                    HomeScreen(
-                        viewModel = homeViewModel,
-                        onViewTripClick = { selectedTripId ->
-                            backStack.add(
-                                Route.TripOverview(
-                                    selectedTripId
-                                )
-                            )
-                        },
-                        onAddTripClick = { backStack.add(Route.EditTrip(-1)) },
-                        snackbarMessage = snackbarMessage,
-                        onSnackbarShown = { snackbarMessage = null }
-                    )
-                }
-            }
-            entry<Route.TripOverview> { key ->
-                NavEntry(key) {
-                    val tripId = key.id
-                    val viewModelKey = if (tripId == -1) {
-                        "TripDetailVM_New_${UUID.randomUUID()}"
-                    } else {
-                        "TripDetailVM_$tripId"
-                    }
-                    val viewModel: TripDetailsViewModel =
-                        viewModel(
-                            key = viewModelKey,
-                            factory = TripDetailsViewModel.provideFactory(
-                                tripId,
-                                repository
+            entry<TripList> { key ->
+                val homeViewModel: HomeViewModel = viewModel(
+                    factory = HomeViewModel.provideFactory(repository)
+                )
+                HomeScreen(
+                    viewModel = homeViewModel,
+                    onViewTripClick = { selectedTripId ->
+                        backStack.add(
+                            TripOverview(
+                                selectedTripId
                             )
                         )
-                    val start = viewModel.uiState.value.trip.startDate
-                    val endDate = viewModel.uiState.value.trip.endDate
-                    TripOverviewScreen(
-                        viewModel = viewModel,
-                        onNavigateUp = { result ->
-                            if (result == "deleted") {
-                                snackbarMessage = "Trip Deleted"
-                            }
-                            backStack.removeLastOrNull()
-                        },
-                        onAddReceiptClick = { currencyCode ->
-                            backStack.add(
-                                Route.AddReceipt(
-                                    tripId,
-                                    currencyCode
-                                )
-                            )
-                        },
-                        onEditTripClick = { backStack.add(Route.EditTrip(tripId)) },
-                        onNavigateToReceipt = { receiptId, currencyCode ->
-                            backStack.add(
-                                Route.EditReceipt(
-                                    receiptId,
-                                    start,
-                                    endDate,
-                                    currencyCode
-                                )
-                            )
-                        },
-                        snackbarMessage = snackbarMessage,
-                        onSnackbarShown = { snackbarMessage = null }
-                    )
-                }
+                    },
+                    onAddTripClick = { backStack.add(EditTrip(-1)) },
+                    snackbarMessage = snackbarMessage,
+                    onSnackbarShown = { snackbarMessage = null }
+                )
+
             }
-            entry<Route.EditTrip> { key ->
-                NavEntry(key) {
-                    val tripId = key.id
-                    val viewModelKey = if (tripId == -1) {
-                        "TripDetailVM_New_${UUID.randomUUID()}"
-                    } else {
-                        "TripDetailVM_$tripId"
-                    }
-                    val viewModel: TripDetailsViewModel =
-                        viewModel(
-                            key = viewModelKey,
-                            factory = TripDetailsViewModel.provideFactory(
-                                tripId,
-                                repository
-                            )
+            entry<TripOverview> { key ->
+                val tripId = key.id
+                val viewModelKey = if (tripId == -1) {
+                    "TripDetailVM_New_${UUID.randomUUID()}"
+                } else {
+                    "TripDetailVM_$tripId"
+                }
+                val viewModel: TripDetailsViewModel =
+                    viewModel(
+                        key = viewModelKey,
+                        factory = TripDetailsViewModel.provideFactory(
+                            tripId,
+                            repository
                         )
-                    EditTripScreen(
-                        viewModel = viewModel,
-                        onNavigateUp = { result ->
-                            when (result) {
-                                "saved" -> {
-                                    snackbarMessage = "Trip Saved"
-                                    backStack.removeLastOrNull()
-                                }
-
-                                "deleted" -> {
-                                    snackbarMessage = "Trip Deleted"
-                                    backStack.retainAll { it is Route.TripList }
-                                }
-
-                                else -> backStack.removeLastOrNull()
-                            }
-                        },
                     )
-                }
-            }
-            entry<Route.AddReceipt> { key ->
-                NavEntry(key) {
-                    val tripId = key.tripId
-                    val currencyCode = key.currencyCode
-                    val viewModel: TripDetailsViewModel = viewModel(
-                        // Reusing Trip ViewModel
-                        key = "TripDetailVM_$tripId",
-                        factory = TripDetailsViewModel.provideFactory(tripId, repository)
-                    )
-                    AddReceiptScreen(
-                        viewModel = viewModel,
-                        currencyCode = currencyCode,
-                        onNavigateUp = { result ->
-                            if (result == "saved") {
-                                snackbarMessage = "Receipt Saved"
-                            }
-                            backStack.removeLastOrNull()
+                val uiState by viewModel.uiState.collectAsState()
+                TripOverviewScreen(
+                    viewModel = viewModel,
+                    onNavigateUp = { result ->
+                        if (result == "deleted") {
+                            snackbarMessage = "Trip Deleted"
                         }
-                    )
-                }
+                        backStack.removeLastOrNull()
+                    },
+                    onAddReceiptClick = { currencyCode ->
+                        backStack.add(
+                            AddReceipt(
+                                tripId,
+                                currencyCode
+                            )
+                        )
+                    },
+                    onEditTripClick = { backStack.add(EditTrip(tripId)) },
+                    onNavigateToReceipt = { receiptId, currencyCode ->
+                        backStack.add(
+                            EditReceipt(
+                                receiptId,
+                                uiState.trip.startDate,
+                                uiState.trip.endDate,
+                                currencyCode
+                            )
+                        )
+                    },
+                    snackbarMessage = snackbarMessage,
+                    onSnackbarShown = { snackbarMessage = null }
+                )
+
             }
-            entry<Route.EditReceipt> { key ->
-                NavEntry(key) {
-                    val receiptId = key.receiptId
-                    val tripStartDate = key.tripStartDate
-                    val tripEndDate = key.tripEndDate
-                    val currencyCode = key.currencyCode
-                    val viewModel: ReceiptViewModel = viewModel(
-                        key = "ReceiptDetailVM_$receiptId",
-                        factory = ReceiptViewModel.provideFactory(receiptId, repository)
-                    )
-                    EditReceiptDataScreen(
-                        viewModel = viewModel,
-                        windowSize = windowSize,
-                        tripStartDate = tripStartDate,
-                        tripEndDate = tripEndDate,
-                        currencyCode = currencyCode,
-                        onNavigateUp = { backStack.removeLastOrNull() }
-                    )
+            entry<EditTrip> { key ->
+                val tripId = key.id
+                val viewModelKey = if (tripId == -1) {
+                    "TripDetailVM_New_${UUID.randomUUID()}"
+                } else {
+                    "TripDetailVM_$tripId"
                 }
+                val viewModel: TripDetailsViewModel =
+                    viewModel(
+                        key = viewModelKey,
+                        factory = TripDetailsViewModel.provideFactory(
+                            tripId,
+                            repository
+                        )
+                    )
+                EditTripScreen(
+                    viewModel = viewModel,
+                    onNavigateUp = { result ->
+                        when (result) {
+                            "saved" -> {
+                                snackbarMessage = "Trip Saved"
+                                backStack.removeLastOrNull()
+                            }
+
+                            "deleted" -> {
+                                snackbarMessage = "Trip Deleted"
+                                backStack.retainAll { it is TripList }
+                            }
+
+                            else -> backStack.removeLastOrNull()
+                        }
+                    },
+                )
+            }
+            entry<AddReceipt> { key ->
+                val tripId = key.tripId
+                val currencyCode = key.currencyCode
+                val viewModel: TripDetailsViewModel = viewModel(
+                    // Reusing Trip ViewModel
+                    key = "TripDetailVM_$tripId",
+                    factory = TripDetailsViewModel.provideFactory(tripId, repository)
+                )
+                AddReceiptScreen(
+                    viewModel = viewModel,
+                    currencyCode = currencyCode,
+                    onNavigateUp = { result ->
+                        if (result == "saved") {
+                            snackbarMessage = "Receipt Saved"
+                        }
+                        backStack.removeLastOrNull()
+                    }
+                )
+            }
+            entry<EditReceipt> { key ->
+                val receiptId = key.receiptId
+                val tripStartDate = key.tripStartDate
+                val tripEndDate = key.tripEndDate
+                val currencyCode = key.currencyCode
+                val viewModel: ReceiptViewModel = viewModel(
+                    key = "ReceiptDetailVM_$receiptId",
+                    factory = ReceiptViewModel.provideFactory(receiptId, repository)
+                )
+                EditReceiptDataScreen(
+                    viewModel = viewModel,
+                    windowSize = windowSize,
+                    tripStartDate = tripStartDate,
+                    tripEndDate = tripEndDate,
+                    currencyCode = currencyCode,
+                    onNavigateUp = { backStack.removeLastOrNull() }
+                )
             }
         }
     )
