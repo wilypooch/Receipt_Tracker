b2b6f975eec1e24a9fb7d804be4aa98c95652cbb
Stephen Jansen <156315560+wilypooch@users.noreply.github.com>
2026-04-01 14:57:27 +0100

    rename namespace and applicationId from example.com

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.receipttracker"
+    namespace = "uk.wilypooch.receipttracker"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.receipttracker"
+        applicationId = "uk.wilypooch.receipttracker"
         minSdk = 29
         targetSdk = 36
         versionCode = 1
